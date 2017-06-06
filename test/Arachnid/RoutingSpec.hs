d1e9b654059c7670ce425ece3c34968ca3aa478f
Pete Johanson <peter@peterjohanson.com>
2017-06-06 00:41:34 -0400

    Spec tweaks.

--- test/Arachnid/RoutingSpec.hs
+++ test/Arachnid/RoutingSpec.hs
@@ -4,16 +4,21 @@ import Test.Hspec
 import Arachnid.Routing
 
 spec :: Spec
-spec =
+spec = do
   describe "Routing" $ do
-    describe "A basic string segment" $
+    describe "A basic string segment" $ do
       it "matches the same string route" $ do
         let isJust (Just _) = True
             isJust _ = False
         match (Segment "home") "home" `shouldSatisfy` isJust
 
-    describe "A capture segment" $
-      it "Captures the value" $
+      it "does not match a different string" $ do
+        let isNothing Nothing = True
+            isNothing _ = False
+        match (Segment "home") "users" `shouldSatisfy` isNothing
+
+    describe "A capture segment" $ do
+      it "Captures the value" $ do
         captures `shouldBe` Just [("name", "John")]
         where
           captures = routeMatchCaptures `fmap` match ("home" </> Capture "name") "home/John"
