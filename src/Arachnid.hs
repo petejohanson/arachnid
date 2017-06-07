695a4b12427134c918dea86680b8bd1dee8c6dc3
Pete Johanson <peter@peterjohanson.com>
2017-06-07 00:38:37 -0400

    Add fallback 404 handler if no route matches.

--- src/Arachnid.hs
+++ src/Arachnid.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Arachnid
 ( makeApp
 ) where
@@ -8,13 +10,15 @@ import Arachnid.Types
 
 import Control.Monad.Trans.Resource
 
+import qualified Network.HTTP.Types as HTTP
 import qualified Network.Wai as W
 
 makeApp :: (Resource a) => [(Route, a)] -> W.Application
-makeApp (r:rs) req =
+makeApp [] _ respond = respond $ W.responseLBS HTTP.notFound404 [] "Not Found"
+makeApp (r:rs) req respond =
   case match (fst r) path of
-    Just _  -> runHandler (snd r) req
-    Nothing -> makeApp rs req
+    Just _  -> runHandler (snd r) req respond
+    Nothing -> makeApp rs req respond
   where path = W.pathInfo req
 
 runHandler :: (Resource a) => a -> W.Request -> (W.Response -> IO W.ResponseReceived) -> IO W.ResponseReceived
