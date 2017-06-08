fd4d8b5a4d076fdebf46d7f256bce9568cdc8da1
Pete Johanson <peter@peterjohanson.com>
2017-06-07 21:44:01 -0400

    More tests.

--- test/Arachnid/RoutingSpec.hs
+++ test/Arachnid/RoutingSpec.hs
@@ -7,6 +7,17 @@ import Data.Text (pack)
 spec :: Spec
 spec = do
   describe "Routing" $ do
+    describe "The root route" $ do
+      it "matches the basic route" $ do
+        let isJust (Just _) = True
+            isJust _ = False
+        match [Root] [] `shouldSatisfy` isJust
+
+      it "does not match complex routes" $ do
+        let isNothing Nothing = True
+            isNothing _ = False
+        match [Root] [pack "home"] `shouldSatisfy` isNothing
+
     describe "A basic string segment" $ do
       it "matches the same string route" $ do
         let isJust (Just _) = True
